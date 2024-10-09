e125a3aabbcaaea3f2a02e75fed2dc84a7ee9d01
unknown <your@email.example>
2024-10-09 12:29:50 +0300

    first commit

---
+++ web/blog.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="index.php">Indekss</a>
+    <h1>Tiešsaistes dienasgrāmata</h1>
+
+    <div>
+        <h2 class="small-heading">Biogrāfija</h2>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sit distinctio, veritatis debitis veniam eos? Laudantium recusandae iure perspiciatis amet illum, pariatur ad exercitationem esse. Explicabo consectetur, rerum voluptate sit ab nihil ad laudantium mollitia molestias esse natus possimus voluptatem illo itaque perspiciatis repellendus harum magnam. Consequatur ipsam excepturi maiores, iusto magni perferendis aspernatur recusandae officia possimus cumque praesentium officiis, veritatis blanditiis facere sed deleniti suscipit quisquam. Omnis dolor eos labore sequi, eum unde. Iure recusandae reprehenderit incidunt sequi rem ad, tempore ipsam nostrum, maiores nulla nihil eius delectus magni accusamus quisquam perferendis? Molestiae iure ab vel earum, ullam nobis.
+    </div>
+
+    <div>
+        <h2 class="small-heading">Kapēc vēlies kļūt par programētāju</h2>
+        <h2></h2>
+        <ul>
+            <li>1. iemesls</li>
+            <li>2. iemesls</li>
+            <li>3. iemesls</li>
+            <li>4. iemesls</li>
+            <li>5. iemesls</li>
+        </ul>
+    
+    </div>
+</body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="blog.php">Blogs</a>
+    <h1>Piesakies webināram!</h1>
+
+    <form action="" method="post">
+        <label for="vards">Vārds:</label>
+        <input type="text" name="" id="vards">
+        <br>
+        
+        <label for="epasts">Epasts:</label>
+        <input type="email" name="" id="epasts">
+        <br>
+
+        <select name="level">
+            <option value="">--Please choose an level--</option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+        </select>
+        <br>
+        <legend>Vai vēlies saņemt informāciju par....</legend>
+        <input type="radio" id="info_yes" name="info_choice" value="yes" />
+        <label for="info_yes">Yes</label>
+
+        <input type="radio" id="info_no" name="info_choice" value="no" />
+        <label for="info_no">No</label>
+        <br>
+        <input type="submit" value="Sūtīt">
+    </form>
+
+    <?php
+        $names=[
+            ["name" => "Janis", "email" => "email@email.com", "level" => "beginner"],
+            ["name" => "Aivars", "email" => "email@email.com", "level" => "beginner"],
+            ["name" => "Kristaps", "email" => "email@email.com", "level" => "beginner"],
+        ];
+
+        foreach($names as $name){
+            echo "Vārds:" . $name["name"] . " , Email:" . $name["email"] . " Level: " . $name["level"] . "<br>";
+        }
+    ?>
+
+    <script src="js/app.js"></script>
+</body>
+</html>
